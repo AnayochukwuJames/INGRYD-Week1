6c394ee68f815bee95fba24b8504a76fba8db597
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-03-02 12:57:42 +0100

    Update initial commit

---
+++ src/main/java/org/example/week3/assessment/Horse.java
@@ -0,0 +1,31 @@
+package org.example.week3.assessment;
+
+public class Horse extends LivingThings{
+    public Horse(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Can Horse speak");
+
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Horse can sleep");
+
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Horse can move");
+
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Horse eat");
+
+    }
+}
---
+++ src/main/java/org/example/week3/assessment/Human.java
@@ -0,0 +1,13 @@
+package org.example.week3.assessment;
+
+public class Human implements Living{
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+}
